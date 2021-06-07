1edd13016ac1389f65894a877fb1bae64533bd20
Janusz Dlugosz <jandlu08@gmail.com>
2021-06-07 13:33:58 +0200

    add frontend

--- backend/MoneyApi/Identity/Models/AppUser.cs
+++ backend/MoneyApi/Identity/Models/AppUser.cs
@@ -1,8 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace MoneyApi.Identity.Models
 {
     public class AppUser : IdentityUser
     {
-        // Add additional profile data for application users by adding properties to this class
         public string Name { get; set; }
     }
 }
